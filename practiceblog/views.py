792cba319ddc84bd8eb235267b987c6b45ac894a
Keven <kevenmolina@gmail.com>
2019-06-09 14:52:10 -0500

    Created about and Home routes. Welcome to 742 Evergreen Terrace

---
+++ practiceblog/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+#allows pages to be rendered
+from django.shortcuts import render
+
+def home(request):
+    # return(request, 'home/')
+    return render(request,'home.html')
+
+def about(request):
+    return HttpResponse('about')
