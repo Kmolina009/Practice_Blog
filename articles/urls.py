bb97aefdde9827964d88465b51079965e47b06fc
Keven <kevenmolina@gmail.com>
2019-06-09 19:36:45 -0500

    Figured out how to properly access articles page

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #When adding accessing this page  => http://localhost:8080/articles/articles/  
     path('articles/', views.article_list),
 ]
